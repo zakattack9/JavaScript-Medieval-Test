5f8040ab05011d51d93631d960e1f496ca3d9729
zakattack9 <zakattack26111@gmail.com>
2016-11-23 08:46:39 -1000

    Medieval Times

---
+++ test_review.js
@@ -0,0 +1,46 @@
+//Variables
+var knight = "Gwain";
+var maiden = "Guinevere";
+var monster = "Griffin";
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
+var hero = {
+  wizard: "Merlin",
+  weapon: weaponChest[2], 
+  power: 16
+};
+
+//Hail the King
+function swordInTheStone(worthy){
+  if(worthy == "Arthur"){
+    console.log("We have found the King worthy.");
+  }
+  else{
+    console.log("We must keep searching, for " + worthy + " is unworthy.");
+  }
+}
+swordInTheStone(knight);
+
+//Hail the King and Queen
+function trueLove(king, queen){
+  if(queen == "Guinevere" && king == "Arthur"){
+    console.log("On this day, our " + king + " and " + queen + " shall rule happily ever after.");
+  }
+  else{
+    console.log("We must keep searching, for true love must reign over Camelot.");
+  }
+}
+trueLove(knight, maiden);
+
+//Merlin and Monster
+function epicBattle(wizard, enemy, power){
+  if(power <= 15){
+    console.log("Our " + wizard + " has fallen.");
+  }
+  else if(power >= 16 && power <= 40){
+    console.log(wizard + " has almost slain the " + enemy + ".");
+  }
+  else{
+    console.log("Our " + wizard + " has slain the enemy!");
+  }
+}
+epicBattle(hero.wizard, monster, hero.power);
